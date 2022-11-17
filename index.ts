849a05b0452876b296c20daaeca7aa5b3ae89447
Chuck Lam <939400+chucklam@users.noreply.github.com>
2022-11-17 11:06:48 -0800

    Analyse transactions instead of execution outcomes

--- index.ts
+++ index.ts
@@ -1,18 +1,24 @@
 #!/usr/bin/env ts-node
 
 import { startStream, types } from 'near-lake-framework';
+import { exit } from 'process';
 
 const lakeConfig: types.LakeConfig = {
   s3BucketName: "near-lake-data-mainnet",
   s3RegionName: "eu-central-1",
-  startBlockHeight: 63804051,
+  startBlockHeight: 78648779,
 };
 
-interface EventLogData {
-  standard: string, // 'nep141'
-  version: string,  // '1.0.0'
-  event: string,    // 'ft_transfer'
-  data?: unknown,
+// interface EventLogData {
+//   standard: string, // 'nep141'
+//   version: string,  // '1.0.0'
+//   event: string,    // 'ft_transfer'
+//   data?: unknown,
+// };
+
+const decodeBase64 = (base64: string): string => {
+  const buff = Buffer.from(base64, 'base64');
+  return buff.toString('utf8');
 };
 
 async function handleStreamerMessage(
@@ -20,44 +26,29 @@ async function handleStreamerMessage(
 ): Promise<void> {
   const blockHeader = streamerMessage.block.header;
   const createdOn = new Date(blockHeader.timestamp / 1000000);
-
-  // Collect Receipts and ExecutionOutcomes from StreamerMessage
-  const receiptExecutionOutcomes = streamerMessage
-    .shards
-    .flatMap(shard => shard.receiptExecutionOutcomes);
-
-  // Extract Outcomes with Event
-  const outcomesWithEvents = receiptExecutionOutcomes
-    .map(outcome => ({
-      createdOn,
-      receipt: {
-        id: outcome.receipt?.receiptId,
-        receiverId: outcome.receipt?.receiverId,
-      },
-      events: outcome.executionOutcome.outcome.logs.map(
-        (log: string): EventLogData | undefined => {
-          const [_, probablyEvent] = log.match(/^EVENT_JSON:(.*)$/) ?? []
-          try {
-            return JSON.parse(probablyEvent)
-          } catch (e) {
-            return
-          }
-        }
-      )
-      .filter(event => event !== undefined)
-    }))
   
-  // Filter for relevant events
-  const relevantOutcomes = outcomesWithEvents
-    .filter(outcome =>
-      outcome.events.some(
-        // event => event?.standard === "nep171" && event.event === "nft_mint"
-        event => event?.standard === 'nep141' && event.event === 'ft_transfer'
-      )
-    );
+  const blockHeight = blockHeader.height;
+  // if (blockHeight > 78648779) process.exit();
 
-  if (relevantOutcomes.length > 0) {
-    relevantOutcomes.forEach(outcome => console.dir(outcome, { depth: 5 }));
+  const relevantTxs = streamerMessage
+    .shards
+    .flatMap(shard => shard.chunk?.transactions)
+    .map(tx => tx?.transaction)
+    // USDC contract
+    .filter(tx => tx?.receiverId === 'a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.factory.bridge.near')
+    .map(tx => ({
+      ...tx,
+      actions: tx?.actions && [...tx?.actions.map((action: any) => ({
+        ...action,
+        FunctionCall: {
+          ...action?.FunctionCall,
+          arguments: JSON.parse(decodeBase64(action?.FunctionCall.args)),
+        }
+      }))]
+    }));
+
+  if (relevantTxs.length > 0) {
+    relevantTxs.forEach(tx => console.dir(tx, { depth: 6 }));
   }
 }
 
